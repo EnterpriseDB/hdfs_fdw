6040523ac32aa0377fdb6a6f2a368be07bd5a8ab
Abbas Butt <abbas.butt@enterprisedb.com>
2017-11-20 15:15:10 +0500

    Fix fo RM 42833 Add test cases for date comparison in where clause

---
+++ sql/date_comparison.sql
@@ -0,0 +1,87 @@
+/*---------------------------------------------------------------------------------------------------------------------
+ *
+ * mapping.sql
+ * 		Foreign-data wrapper for remote Hadoop servers
+ *              To verify that Date comparisions are used to retrive data correctly from Hadoop Foreign Tables.
+ *
+ * Portions Copyright (c) 2012-2014, PostgreSQL Global Development Group
+ *
+ * Portions Copyright (c) 2004-2014, EnterpriseDB Corporation.
+ *
+ * IDENTIFICATION
+ * 		date_comparison.sql
+ *
+ *---------------------------------------------------------------------------------------------------------------------
+ */
+
+
+-- Connection Settings.
+
+\set HIVE_SERVER         `echo \'"$HIVE_SERVER"\'`
+\set HIVE_CLIENT_TYPE           '\'hiveserver2\''
+\set HIVE_PORT           `echo \'"$HIVE_PORT"\'`
+\set HIVE_USER           `echo \'"$HIVE_USER"\'`
+\set HIVE_PASSWORD       `echo \'"$HIVE_PASSWORD"\'`
+
+-- Create the database.
+
+CREATE DATABASE fdw_regression;
+
+\c fdw_regression postgres
+
+-- Set the Date Style
+
+SET datestyle TO ISO,DMY;
+
+-- Set the Search Path to PUBLIC Schema
+
+SET search_path TO public;
+
+-- Create Hadoop FDW Extension.
+
+CREATE EXTENSION hdfs_fdw;
+
+-- Create Hadoop FDW Server.
+
+CREATE SERVER hdfs_server FOREIGN DATA WRAPPER hdfs_fdw OPTIONS(host :HIVE_SERVER, port :HIVE_PORT, client_type :HIVE_CLIENT_TYPE);
+
+-- Create Hadoop USER MAPPING.
+
+CREATE USER MAPPING FOR postgres SERVER hdfs_server OPTIONS (username :HIVE_USER, password :HIVE_PASSWORD);
+
+-- Create Foreign Tables.
+
+CREATE FOREIGN TABLE emp (
+    empno           INTEGER,
+    ename           VARCHAR(10),
+    job             VARCHAR(9),
+    mgr             INTEGER,
+    hiredate        DATE,
+    sal             INTEGER,
+    comm            INTEGER,
+    deptno          INTEGER
+)
+SERVER hdfs_server OPTIONS (dbname 'fdw_db', table_name 'emp');
+
+SELECT * FROM emp;
+
+-- Retive data based on date comparisons.
+
+SELECT * FROM emp
+WHERE hiredate = '1980-12-17';
+
+SELECT * FROM emp
+WHERE hiredate < '23/01/1982';
+
+SELECT * FROM emp
+WHERE hiredate BETWEEN '20/02/1981' AND '23/01/1982';
+
+
+
+--Cleanup
+
+DROP EXTENSION hdfs_fdw CASCADE;
+
+\c postgres postgres
+
+DROP DATABASE fdw_regression;
