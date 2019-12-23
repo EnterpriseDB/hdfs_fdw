5a5a86cbbb7c9274399ba4aefdd45dd1eefe45d3
Kuntal Ghosh <kuntal.ghosh@enterprisedb.com>
2019-12-23 17:50:13 +0530

    Add support to update extensions from lower versions to 2.0.4/5

---
+++ hdfs_fdw--2.0.3--2.0.4.sql
@@ -0,0 +1,3 @@
+/* hdfs_fdw/hdfs_fdw--2.0.3--2.0.4.sql */
+
+-- No new functions or definitions were added in 2.0.4
---
+++ hdfs_fdw--2.0.4--2.0.5.sql
@@ -0,0 +1,3 @@
+/* hdfs_fdw/hdfs_fdw--2.0.4--2.0.5.sql */
+
+-- No new functions or definitions were added in 2.0.5
